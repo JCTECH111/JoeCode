338131e84de07c2bbbb3e4a0f749ddba36032ebe
jctech111 <jctech333@gmail.com>
2025-04-27 00:04:31 +0100

    working on the nav

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
-// import About from "../pages/About";
+import About from "../pages/About";
 // import Projects from "../pages/Projects";
 // import Contact from "../pages/Contact";
 import NotFound from "../pages/NotFound";
@@ -12,9 +12,9 @@ export default function AppRoutes() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} /> */}
+         <Route path="/about" element={<About />} />
+        {/* <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />  */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
