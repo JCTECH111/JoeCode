88d3cd995bfb2d65500f57faf5a4a779eb3b3b56
jctech111 <jctech333@gmail.com>
2025-04-27 18:20:20 +0100

    working on the marquee movement

--- src/components/PortfolioMarquee.jsx
+++ src/components/PortfolioMarquee.jsx
@@ -1,16 +1,18 @@
 import Marquee from "react-fast-marquee";
 import avatar from "../assets/avatar.png"
+import project_1 from "../assets/project-1.png"
+import project_2 from "../assets/project-2.png"
 
 function PortfolioMarquee() {
   return (
     <div className="my-12 absolute md:top-[17cm] top-[12cm]  w-full overflow-hidden  p-4">
       <Marquee speed={40} pauseOnHover gradient={false}>
-        {[avatar, avatar, avatar, avatar].map((img, index) => (
-          <div key={index} className="mx-4 w-[300px] h-[180px] overflow-hidden rounded-lg shadow-lg p-4">
+        {[avatar, project_1, project_2, project_1].map((img, index) => (
+          <div key={index} className="mx-4 w-[350px] h-[200px] overflow-hidden rounded-lg shadow-lg p-2">
             <img 
               src={img} 
               alt={`Project ${index + 1}`} 
-              className="w-full h-full object-cover hover:scale-105 transition-transform"
+              className="w-full h-full object-full hover:scale-105 transition-transform"
             />
           </div>
         ))}
