de3e718a5d4f43e0ff45c3e1bf94167a9ba4ac19
jctech111 <jctech333@gmail.com>
2025-04-30 17:10:12 +0100

    working on the contact components

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import contactButtonBg from '../assets/button-image.jpeg'; // Update with your i
 
 function Contact() {
   return (
-    <div className='bg-white mt-20 md:ml-16 max-w-4xl rounded-xl shadow-md'>
+    <div className='bg-white mt-20 md:ml-16 max-w-4xl rounded-xl '>
       <h2 className='text-3xl md:text-4xl font-bold text-gray-800 mb-6'>Get in touch</h2>
       
       <p className='text-gray-600 mb-8 leading-relaxed'>
