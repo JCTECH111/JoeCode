5ccc41247c326583702d82fac4a4f5dab6d5865d
jctech111 <jctech333@gmail.com>
2025-04-30 21:47:35 +0100

    working on the project components

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,14 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Index from "../pages/Index";
 import NotFound from "../pages/NotFound";
 import Nav from "../components/Nav";
-import ProjectDetail from './ProjectDetails';
+import ProjectDetail from '../pages/ProjectDetails';
 export default function AppRoutes() {
   return (
     <Router>
       <Nav />
       <Routes>
         <Route path="/" element={<Index />} />
-        <Route path="/" element={<Index />} />
+        <Route path="/projects/:id" element={<ProjectDetail />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
