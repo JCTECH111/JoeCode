84fa6e4e6982f2404439c993fc339ba5168be30d
jctech111 <jctech333@gmail.com>
2025-05-01 20:53:05 +0100

    working on the meta tag

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -60,7 +60,7 @@ function Contact() {
   };
 
   return (
-    <div className='bg-white mt-20 md:ml-16 max-w-4xl rounded-xl p-6'>
+    <div className='bg-white mt-20 md:ml-16 max-w-4xl rounded-xl p-y6'>
       <h2 className='text-3xl md:text-4xl font-bold text-gray-800 mb-6'>Get in touch</h2>
       
       <p className='text-gray-600 mb-8 leading-relaxed'>
