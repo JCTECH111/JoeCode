0c24921facd32184fde38c5fcf3327d6450328e3
jctech111 <jctech333@gmail.com>
2025-05-01 17:50:39 +0100

    working on the backend

--- backend/Mailer.php
+++ backend/Mailer.php
@@ -1,15 +1,16 @@
 <?php
 ob_start();
+
 header("Access-Control-Allow-Origin: https://joecode.vercel.app");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Access-Control-Allow-Credentials: true");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit;
-}
+};
 
 require './vendor/autoload.php'; // Load PHPMailer
 use PHPMailer\PHPMailer\PHPMailer;
